abbdb5d532ada30f7e9dd6057934702c4a28728f
Tiago de Castro Lima <casmei@protonmail.com>
2022-05-19 18:20:51 -0300

    Tira lib Rich

--- interface.py
+++ interface.py
@@ -6,14 +6,19 @@ from models.ListaProduto import ListaProduto
 from models.Entrada import Entrada
 from models.Produto import Produto
 
-
-from rich.console import Console
-from rich.table import Table
-
 menu = MenuManager()
 estoque = Estoque()
 
-print = Console().print
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+UNDERLINE = "\033[4m"
+RESET = "\033[0m"
 
 # * ######### PRODUTOS #########
 def criar_produtos():
@@ -22,25 +27,30 @@ def criar_produtos():
     chamar a função de cadastro
 
     """
-    flag = True
+    print("=" * 70)
     while True:
         nome = input("Nome do produto: ")
         continuar = input("Deseja adicionar outro: (s/n) ")
         produto = Produto(nome)
         estoque.cadastrar_produto(produto)
+        print(f"{GREEN}Produto Cadastrado com Sucesso!{RESET}")
         print()
-        print("Produto Cadastrado com Sucesso!")
-        flag = continuar == "s"
+        if continuar.lower() == "n":
+            break
+    print("=" * 70)
 
 
 def req_deletar_produto():
+    print("=" * 70)
+    # TODO FAZER A VALIDAÇÃO DO NOME DO PRODUTO
     produto = input("Digite o nome do produto que deseja deletar: ")
     confirmacao = input(f"Realmente deseja excluir o produto {produto}?: (s/n) ")
     if "s" == confirmacao.lower():
         estoque.deletar_produto(produto)
-        print("Produto deletado com sucesso!")
+        print(f"{GREEN}Produto deletado com sucesso!{RESET}")
     else:
-        return "Deleção cancelada!"
+        print(f"{RED}Deleção cancelada!{RESET}")
+    print("=" * 70)
 
 
 def listar_produtos():
@@ -48,15 +58,18 @@ def listar_produtos():
     Pecorrer o Array de produtos no
     arquivo, e retorna-los.
     """
-    table = Table(show_header=True, show_lines=False, header_style="bold red")
-
-    table.add_column("ID", justify="center", style="bold green")
-    table.add_column("Nome", justify="center", style="bold green")
-
-    for i in estoque.produtos:
-        table.add_row(i.id, i.name)
-
-    print(table)
+    print("=" * 70)
+    print("Listagem de Produtos")
+    if len(estoque.produtos) == 0:
+        print(f"{RED}Não há produtos cadastrados!{RESET}")
+    else:
+        for i in estoque.produtos:
+            espaço_id = gerar_espacos(i.id, 32)
+            espaço_name = gerar_espacos(i.name, 12)
+            print(
+                f"| {BLUE}{i.name}{RESET} {espaço_name} | {YELLOW}{i.id}{RESET} {espaço_id}"
+            )
+    print("=" * 70)
 
 
 def produto_pelo_nome():
@@ -66,43 +79,35 @@ def produto_pelo_nome():
     nome = input("Digite o nome do produto que deseja buscar: ")
     produto = estoque.retorna_produto(nome)
 
-    print()
+    print("=" * 70)
     if produto == None:
-        print("Produto não existe")
+        print(f"{RED}Produto não existe{RESET}")
     else:
-        print(f"ID do produto: {produto.id}")
-        print(f"Nome do produto: {produto.name}")
+        print(f"ID do produto: {YELLOW}{produto.id}{RESET}")
+        print(f"Nome do produto: {YELLOW}{produto.name}{RESET}")
+    print("=" * 70)
 
 
 def produto_pelo_id():
     """
     retorna um produto pelo ID.
     """
+    print("=" * 70)
     id = input("Digite o ID do produto que deseja buscar: ")
     produto = estoque.retorna_produto(id)
 
     print()
     if produto == None:
-        print("Produto não existe")
+        print("{RED}Produto não existe{RESET}")
     else:
-        print(f"ID do produto: {produto.id}")
-        print(f"Nome do produto: {produto.name}")
+        print(f"ID do produto: {YELLOW}{produto.id}{RESET}")
+        print(f"Nome do produto: {YELLOW}{produto.name}{RESET}")
+    print("=" * 70)
 
 
 # * ####### ENTRADAS #########
 def criar_entrada():
-    """
-    Cria uma entrada no JSON
-
-    - Pega a nota fiscal da entrada
-    - Cria uma lista ```produtos```
-    - Loop para adicionar os produtos na lista
-        -  Vai pegar o nome do produto e verificar se já existe na lista
-        -  Pegar quantidade desse produto
-        -  Adicionalos na lista de produtos
-        -  Verifico se desejo adicionar outro
-    - Cadastro minha entrada, com a nf e uma lista de produtos
-    """
+    print("=" * 70)
     nf = input("Qual a nota fiscal da entrada: ")
     produtos = StaticList(0)
     while True:
@@ -114,8 +119,9 @@ def criar_entrada():
         produto = estoque.produtos.find(comparador)
 
         if produto == None:
-            print("Produto não existe")
-            break
+            print(f"{RED}Produto não existe{RESET}")
+            print("=" * 70)
+            return
 
         quantidade = int(input(f"Quantidade de {nome}: "))
         produtos = produtos.add((produto, quantidade))
@@ -124,135 +130,144 @@ def criar_entrada():
             break
 
     estoque.cadastrar_entrada(Entrada(nf, ListaProduto(produtos)))
-    print("Entrada cadastrada com sucesso!")
+    print(f"{GREEN}Entrada cadastrada com sucesso!{RESET}")
+    print("=" * 70)
 
 
 def listar_entrada():
-    for i in estoque.entradas:
-        table = Table(
-            title=f"Entrada {i.nf}",
-            show_header=True,
-            header_style="bold red",
-        )
+    print("=" * 70)
 
-        table.add_column("Produto", justify="center", style="bold green")
-        table.add_column("Quantidade", justify="center", style="bold green")
+    if len(estoque.entradas) == 0:
+        print(f"{RED}Não há entradas cadastradas!{RESET}")
+        print("=" * 70)
+    else:
+        for i in estoque.entradas:
+            print()
+            print(f"Nota fiscal da entrada: {YELLOW}{i.nf}{RESET}")
+            print(f"Data da entrada: {YELLOW}{i.createdAt.strftime('%d-%m-%Y')}{RESET}")
+            print()
+            for produto, quantidade in i.lista_produto:
+                espaco_produto = gerar_espacos(produto.name, 15)
+                espaco_quantidade = gerar_espacos(str(quantidade), 15)
+                print(
+                    f"{produto.name} {espaco_produto} | {quantidade} {espaco_quantidade}"
+                )
 
-        for produto, quantidade in i.lista_produto:
-            table.add_row(produto.name, str(quantidade))
+            if len(estoque.entradas) > 1:
+                print()
+                print("*" * 30)
+        print()
+        print("=" * 70)
 
-        print(table)
+
+def gerar_espacos(item, tamanho: int):
+    return ((len(item) - tamanho) * -1) * " "
 
 
 def entrada_pela_nf():
     """
     retorna um entrada pela sua nota fiscal.
     """
+    print("=" * 70)
     nf = input("Digite a nota fiscal da entrada que deseja buscar: ")
     entrada = estoque.retorna_entrada(nf)
-
-    print()
     if entrada == None:
-        print("Entrada não existe")
+        print(f"{RED}Entrada não existe{RESET}")
+        print("=" * 70)
+        return
+    print(f"Nota fiscal: {YELLOW}{entrada.nf}{RESET}")
+    print(f"Data de criação: {YELLOW}{entrada.createdAt.strftime('%d/%m/%Y')}{RESET}")
+    # TODO PRECISO FAZER ESSA LÓGICA
+    print()
+    if not entrada.lista_produto:
+        print(f"{RED}Não há produtos cadastrados nesta entrada!{RESET}")
+        print("=" * 70)
     else:
-        print("=" * 70)
-        print(f"Nota fiscal: {entrada.nf}")
-        print(f"Data de criação: {entrada.createdAt.strftime('%d/%m/%Y')}")
-        print("=" * 70)
-        table = Table(
-            title=f"Entrada {entrada.nf}",
-            show_header=True,
-            header_style="bold red",
-        )
-
-        table.add_column("Produto", justify="center", style="bold green")
-        table.add_column("Quantidade", justify="center", style="bold green")
-
         for produto, quantidade in entrada.lista_produto:
-            table.add_row(produto.name, str(quantidade))
-
-        print(table)
-        print("=" * 70)
+            espaco_produto = gerar_espacos(produto.name, 15)
+            espaco_quantidade = gerar_espacos(str(quantidade), 15)
+            print(f"{produto.name} {espaco_produto} | {quantidade} {espaco_quantidade}")
+    print("=" * 70)
 
 
 # * ####### SAIDA #########
-def criar_saida():
-    nf = input("Qual a nota fiscal da saida: ")
-    produtos = StaticList(0)
-    quantidade = 0
+# def criar_saida():
+#     nf = input("Qual a nota fiscal da saida: ")
+#     produtos = StaticList(0)
+#     quantidade = 0
 
-    while True:
-        nome = input("Nome do produto a dar baixa: ")
-        produto = estoque.produtos.find(lambda produto: produto.name == nome)
+#     while True:
+#         nome = input("Nome do produto a dar baixa: ")
+#         produto = estoque.produtos.find(lambda produto: produto.name == nome)
 
-        if produto == None:
-            print("Produto não existe")
-            return
+#         if produto == None:
+#             print("Produto não existe")
+#             return
 
-        quantidade = int(input(f"Quantidade de {nome} para dar baixa: "))
+#         quantidade = int(input(f"Quantidade de {nome} para dar baixa: "))
 
-        """ def comparador_de_quantidade(era):
-            ...
+#         """ def comparador_de_quantidade(era):
+#             ...
 
-        estoque.entradas.find() """
+#         estoque.entradas.find() """
 
-        # # TODO Lógica de verificar a quantidade ao dar baixa
-        # produtos = produtos.add([(produto, quantidade)])
+#         # # TODO Lógica de verificar a quantidade ao dar baixa
+#         # produtos = produtos.add([(produto, quantidade)])
 
-        if input("Deseja adicionar outro produto?: (s/n) ").lower() == "n":
-            break
+#         if input("Deseja adicionar outro produto?: (s/n) ").lower() == "n":
+#             break
 
-    produtos = produtos.add((produto, quantidade))
-    estoque.cadastrar_saida(Saida(nf, ListaProduto(produtos)))
-    print("Saida cadastrada com sucesso!")
-
-
-def listar_saida():
-    for i in estoque.saidas:
-        table = Table(
-            title=f"Saída {i.nf}",
-            show_header=True,
-            header_style="bold red",
-        )
-
-        table.add_column("Produto", justify="center", style="bold green")
-        table.add_column("Quantidade", justify="center", style="bold green")
-
-        for produto, quantidade in i.lista_produto:
-            table.add_row(produto.name, str(quantidade))
-
-        print(table)
+#     produtos = produtos.add((produto, quantidade))
+#     estoque.cadastrar_saida(Saida(nf, ListaProduto(produtos)))
+#     print("Saida cadastrada com sucesso!")
 
 
-def saida_nf():
-    """
-    retorna uma saída pela sua nota fiscal.
-    """
-    nf = input("Digite a nota fiscal da saida que deseja buscar: ")
-    saida = estoque.retorna_saida(nf)
+# def listar_saida():
+#     for i in estoque.saidas:
+#         table = Table(
+#             title=f"Saída {i.nf}",
+#             show_header=True,
+#             header_style="bold red",
+#         )
 
-    print()
-    if saida == None:
-        print("saida não existe")
-    else:
-        print("=" * 70)
-        print(f"Nota fiscal: {saida.nf}")
-        print(f"Data de criação: {saida.createdAt.strftime('%d/%m/%Y')}")
-        print("=" * 70)
-        table = Table(
-            title=f"Saida {saida.nf}",
-            show_header=True,
-            header_style="bold red",
-        )
+#         table.add_column("Produto", justify="center", style="bold green")
+#         table.add_column("Quantidade", justify="center", style="bold green")
 
-        table.add_column("Produto", justify="center", style="bold green")
-        table.add_column("Quantidade", justify="center", style="bold green")
+#         for produto, quantidade in i.lista_produto:
+#             table.add_row(produto.name, str(quantidade))
 
-        for produto, quantidade in saida.lista_produto:
-            table.add_row(produto.name, str(quantidade))
+#         print(table)
 
-        print(table)
-        print("=" * 70)
+
+# def saida_nf():
+#     """
+#     retorna uma saída pela sua nota fiscal.
+#     """
+#     nf = input("Digite a nota fiscal da saida que deseja buscar: ")
+#     saida = estoque.retorna_saida(nf)
+
+#     print()
+#     if saida == None:
+#         print("saida não existe")
+#     else:
+#         print("=" * 70)
+#         print(f"Nota fiscal: {saida.nf}")
+#         print(f"Data de criação: {saida.createdAt.strftime('%d/%m/%Y')}")
+#         print("=" * 70)
+#         table = Table(
+#             title=f"Saida {saida.nf}",
+#             show_header=True,
+#             header_style="bold red",
+#         )
+
+#         table.add_column("Produto", justify="center", style="bold green")
+#         table.add_column("Quantidade", justify="center", style="bold green")
+
+#         for produto, quantidade in saida.lista_produto:
+#             table.add_row(produto.name, str(quantidade))
+
+#         print(table)
+#         print("=" * 70)
 
 
 # * ########## FERRAMENTAS #############
@@ -296,9 +311,9 @@ menu.add(
         },
         #######################################
         "Saidas": {
-            "Registrar Saída": criar_saida,
-            "Listar Saída": listar_saida,
-            "Buscar Saída": saida_nf,
+            "Registrar Saída": ...,
+            "Listar Saída": ...,
+            "Buscar Saída": ...,
         },
         #######################################
         "Ferramentas": {
